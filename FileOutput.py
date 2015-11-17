dcfd91748a218a5fe72964873f2c29debcec2350
Joseph N Conlin <conlinjn@ldschurch.org>
2015-11-17 16:20:42 -0700

    Added Excel file output and test

--- FileOutput.py
+++ FileOutput.py
@@ -6,6 +6,8 @@ from TheaterDetailPage import TheaterDetail, MovieDetail
 
 import os
 import csv
+# import xlwt
+from openpyxl import Workbook, load_workbook
 
 # Setup common variables
 _defaultFileName = "MovieInfo"
@@ -30,3 +32,76 @@ class WriteCSV:
                 for showTime in movieDetail.get_movie_show_times():
                     csvFile.writerow([theaterDetail.theaterName, movieDetail.movieName, movieDetail.movieImageURL,
                                       showTime])
+
+
+# class WriteExcel:
+#     """Parse a given TheaterDetailPage.TheaterDetail object to write each show time on a single row as below:
+#        <theaterName>, <movieName>, <movieImageURL>, <showTime>
+#        Expect multiple rows per theater / movie combination
+#     """
+#     defaultFileExtension = ".xls"
+#     _excelHeader = ['TheaterName', 'MovieName', 'MovieImageURL', 'ShowTime']
+#
+#     @staticmethod
+#     def write_movie_details(theaterDetail, fileName=_defaultFileName+defaultFileExtension, row=0):
+#         # Setup Excel data objects
+#         wb = xlwt.Workbook()
+#         ws = wb.add_sheet('Movie Info')
+#
+#         # Check for empty file and write headers on first line if empty
+#         try:
+#             # Use try to determine if file does not exist, use if inside try to determine if file exists with no data.
+#             if os.path.getsize(fileName) == 0:
+#                 pass
+#         except FileNotFoundError:
+#             pass
+#         finally:
+#             ws.write(row, 0, WriteExcel._excelHeader[0])
+#             ws.write(row, 1, WriteExcel._excelHeader[1])
+#             ws.write(row, 2, WriteExcel._excelHeader[2])
+#             ws.write(row, 3, WriteExcel._excelHeader[3])
+#             row += 1
+#
+#         for movieDetail in theaterDetail.get_movies_list():
+#                 for showTime in movieDetail.get_movie_show_times():
+#                     ws.write(row, 0, theaterDetail.theaterName)
+#                     ws.write(row, 1, movieDetail.movieName)
+#                     ws.write(row, 2, movieDetail.movieImageURL)
+#                     ws.write(row, 3, showTime)
+#                     row += 1
+#
+#         wb.save(fileName)
+#         return row
+
+class WriteExcel:
+    """Parse a given TheaterDetailPage.TheaterDetail object to write each show time on a single row as below:
+       <theaterName>, <movieName>, <movieImageURL>, <showTime>
+       Expect multiple rows per theater / movie combination
+    """
+    defaultFileExtension = ".xlsx"
+    _excelHeader = ['TheaterName', 'MovieName', 'MovieImageURL', 'ShowTime']
+
+    @staticmethod
+    def write_movie_details(theaterDetail, fileName=_defaultFileName+defaultFileExtension):
+        # Check for empty file and write headers on first line if empty
+        try:
+            # Setup Excel data objects
+            wb = Workbook()
+            # Use try to determine if file does not exist, use if inside try to determine if file exists with no data.
+            if os.path.getsize(fileName) == 0:
+                ws = wb.active
+                ws.append(WriteExcel._excelHeader)
+                wb.save(fileName)
+        except FileNotFoundError:
+            ws = wb.active
+            ws.append(WriteExcel._excelHeader)
+            wb.save(fileName)
+
+        # Load file that now exists and write data to it
+        wb = load_workbook(filename = fileName)
+        ws = wb.active
+        for movieDetail in theaterDetail.get_movies_list():
+                for showTime in movieDetail.get_movie_show_times():
+                    ws.append([theaterDetail.theaterName, movieDetail.movieName, movieDetail.movieImageURL, showTime])
+
+        wb.save(fileName)
--- MovieInfoTests.py
+++ MovieInfoTests.py
@@ -16,6 +16,7 @@ from selenium.common.exceptions import NoSuchElementException
 _headerSearchText = "Salt Lake City"
 _theaterLinkText = "Cinemark"
 _csvFileName = "CSVTest"+FileOutput.WriteCSV.defaultFileExtension
+_excelFileName = "ExcelTest"+FileOutput.WriteExcel.defaultFileExtension
 
 
 class MovieInfoTests(unittest.TestCase):
@@ -36,8 +37,8 @@ class MovieInfoTests(unittest.TestCase):
         currentFileSize = 0
         try:
             currentFileSize = os.path.getsize(_csvFileName)
-        except os.error:
-            # Error is likely due to file not existing which means size 0 so do nothing
+        except FileNotFoundError:
+            # Error is due to file not existing which means size 0 so do nothing
             pass
 
         # TODO: After TheaterDetailPage.TheaterCalendar has index based function, rewrite below in a loop
@@ -65,70 +66,46 @@ class MovieInfoTests(unittest.TestCase):
         newFileSize = os.path.getsize(_csvFileName)
         self.assertLess(currentFileSize, newFileSize, "Did not write new data to csv output file")
 
+    def test_log_excel(self):
+        currentFileSize = 0
+        try:
+            currentFileSize = os.path.getsize(_excelFileName)
+        except FileNotFoundError:
+            # Error is due to file not existing which means size 0 so do nothing
+            pass
 
-# class TheatersTests(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = TestBrowser().get_browser()
-#         # For internal testing purposes, navigate to the theater search results page
-#         self.header = Header(self.driver)
-#         self.header.do_search(_headerSearchText)
-#         self.theaters = Theaters(self.driver)
-#
-#     def tearDown(self):
-#         self.driver.quit()
-#
-#     def test_theaters_list(self):
-#         self.assertNotEqual(0, len(self.theaters.theatersList), "Did not create theaters list")
-#         self.assertNotEqual(0, len(self.theaters.theatersList[0]), "Did not get a valid list of theaters")
-#
-#     def test_click_theater_bad_input_text(self):
-#         self.assertRaises(NoSuchElementException, self.theaters.click_theater, _headerSearchTextNoSpaces)
-#
-#     def test_click_theater_valid_input_text(self):
-#         currentPage = self.driver.current_url
-#         self.theaters.click_theater(_theaterLinkText)
-#         newPage = self.driver.current_url
-#         theaterName = TheaterDetailPage.TheaterDetail(self.driver).theaterName
-#         self.assertNotEqual(currentPage, newPage, "Selecting a theater did not navigate to a new page")
-#         self.assertIn(_theaterLinkText.lower(), theaterName.lower(),
-#                       "Did not end up on theater detail page for selected theater")
-#
-#
-# class TheaterDetailTests(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = TestBrowser().get_browser()
-#         # For internal testing purposes, navigate to a theater details page
-#         self.header = Header(self.driver)
-#         self.header.do_search(_headerSearchText)
-#         self.theaters = Theaters(self.driver)
-#         self.theaters.click_theater(_theaterLinkText)
-#         self.theater = TheaterDetailPage.TheaterDetail(self.driver)
-#         self.theaterCalendar = TheaterDetailPage.TheaterCalendar(self.driver)
-#
-#     def tearDown(self):
-#         self.driver.quit()
-#
-#     def test_change_days(self):
-#         currentPage = self.driver.current_url
-#         self.theaterCalendar.click_today_plus_two()
-#         newPage = self.driver.current_url
-#         self.assertNotEqual(currentPage, newPage, "Selecting a different day did not navigate to a new page")
-#
-#     def test_movies_list_different_days(self):
-#         currentMovieList = self.theater.theaterMoviesList
-#         self.theaterCalendar.click_today_plus_one()
-#         newTheater = TheaterDetailPage.TheaterDetail(self.driver)
-#         newMovieList = newTheater.theaterMoviesList
-#         self.assertNotEqual(currentMovieList[0].movieShowTimeList[0], newMovieList[0].movieShowTimeList[0],
-#                             "Movie date and time from today matches movie date and time from tomorrow")
+        # TODO: After TheaterDetailPage.TheaterCalendar has index based function, rewrite below in a loop
+        # Initial load of self.theater is today so no need to click, just write data.
+        FileOutput.WriteExcel.write_movie_details(self.theater, _excelFileName)
+        self.theaterCalendar.click_today_plus_one()
+        newDay = TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+        self.theaterCalendar.click_today_plus_two()
+        newDay = TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+        self.theaterCalendar.click_today_plus_three()
+        TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+        self.theaterCalendar.click_today_plus_four()
+        newDay = TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+        self.theaterCalendar.click_today_plus_five()
+        newDay = TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+        self.theaterCalendar.click_today_plus_six()
+        newDay = TheaterDetailPage.TheaterDetail(self.driver)
+        FileOutput.WriteExcel.write_movie_details(newDay, _excelFileName)
+
+        newFileSize = os.path.getsize(_excelFileName)
+        self.assertLess(currentFileSize, newFileSize, "Did not write new data to Excel output file")
 
 
 if __name__ == '__main__':
-    # suite = unittest.TestLoader().loadTestsFromTestCase(HeaderTests)
-    testsToRun = [
-        MovieInfoTests,
-        # TheatersTests,
-        # TheaterDetailTests,
-    ]
-    suite = unittest.TestSuite([unittest.TestLoader().loadTestsFromTestCase(test) for test in testsToRun])
+    suite = unittest.TestLoader().loadTestsFromTestCase(MovieInfoTests)
+    # testsToRun = [
+    #     MovieInfoTests,
+    #     # TheatersTests,
+    #     # TheaterDetailTests,
+    # ]
+    # suite = unittest.TestSuite([unittest.TestLoader().loadTestsFromTestCase(test) for test in testsToRun])
     unittest.TextTestRunner(verbosity=2).run(suite)
